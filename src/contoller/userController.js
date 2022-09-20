b5ffcf3403bd29b98fd1a78d96982c663c8bbc81
komal <ranikomal596@gmail.com>
2022-09-20 16:08:31 +0530

    done with some createUser validation

--- src/contoller/userController.js
+++ src/contoller/userController.js
@@ -1,83 +1,109 @@
 const userModel = require("../models/userModel.js");
 const jwt = require("jsonwebtoken");
+const { findOne } = require("../models/userModel.js");
 
 const createUser = async function (req, res) {
-  try {
-    let data = req.body;
+    try {
+        let data = req.body;
+        let { title, name, phone, email, password, address } = data
+        if (Object.keys(data).length == 0) {
+            return res.status(400).send({
+                status: false, message: "body is empty",
+            });
+        }
+        if (!title) { return res.status(400).send({ status: false, message: "please give a title" }) }
+        if (title != "Mr" && title != "Miss" && title != "Mrs") { return res.status(400).send({ status: false, message: "title should be Mr,Miss,Mrs" }) }
+        if (!name) { return res.status(400).send({ status: false, message: "name is mandatory" }) }
+        if (!(/^[a-z ,.'-]+$/i.test(name))) { return res.status(400).send({ status: false, message: "numeric values and special characters not allowed" }) }
+        if (!phone) { return res.status(400).send({ status: false, message: "please enter phone number" }) }
+        if (!/^[6-9]\d{9}$/.test(phone)) { return res.status(400).send({ status: false, message: "invaid mobile number" }) }
+        if (phone.length < 10 || phone.length > 10) {
+            return res.status(400).send({ status: false, message: "Mobile Number should be ten digit" })
+        }
+        let findPhone = await userModel.findOne({ phone: phone })
+        if (findPhone) { return res.status(400).send({ status: false, message: "Phone number should be unique" }) }
+        if (!email) { return res.status(400).send({ status: false, message: "email is mandatory" }) }
+        if (!(/\S+@\S+\.\S+/).test(email)) { return res.status(400).send({ status: false, message: "email contain special charcter" }) }
+        let findEmail = await userModel.findOne({ email: email })
+        if (findEmail) { return res.status(400).send({ status: false, message: "email should be unique" }) }
+        if (!password) { return res.status(400).send({ status: false, message: "password is mandatory" }) }
+        if (password.length < 8 || password.length > 15) { return res.status(400).send({ status: false, message: "password's length must not be less than 8 and greater then 15" }) }
 
-    const userData = await userModel.create(data);
-    res.status(201).send({ status: true, message: userData });
-  } catch (err) {
-    res.status(500).send({ status: false, message: err.message });
-  }
+
+        const userData = await userModel.create(data);
+        res.status(201).send({ status: true, message: userData });
+
+    } catch (err) {
+        res.status(500).send({ status: false, message: err.message });
+    }
 };
 /*------------------------------------------------------------------------------------------------------------------------------------*/
 const loginUser = async function (req, res) {
-  try {
-    data = req.body;
-    let userName = data.email;
-    let password = data.password;
+    try {
+        data = req.body;
+        let userName = data.email;
+        let password = data.password;
 
-    //if give nothing inside req.body
-    if (Object.keys(data).length == 0) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: "Please provide email & password to login.",
-        });
-    }
-    if (Object.keys(data).length > 2) {
-      return res
-        .status(400)
-        .send({ status: false, message: "Only email & password is required." });
-    }
-    //---------------------------------------------//
-    //if no Email inside req.
-    if (!userName) {
-      return res
-        .status(400)
-        .send({ status: false, message: "please provide an Email !" });
-    }
-    //if no password inside req.body
-    if (!password) {
-      return res
-        .status(400)
-        .send({ status: false, message: "please enter password !" });
-    }
-    //-------------------------------------//
+        //if give nothing inside req.body
+        if (Object.keys(data).length == 0) {
+            return res
+                .status(400)
+                .send({
+                    status: false,
+                    message: "Please provide email & password to login.",
+                });
+        }
+        if (Object.keys(data).length > 2) {
+            return res
+                .status(400)
+                .send({ status: false, message: "Only email & password is required." });
+        }
+        //---------------------------------------------//
+        //if no Email inside req.
+        if (!userName) {
+            return res
+                .status(400)
+                .send({ status: false, message: "please provide an Email !" });
+        }
+        //if no password inside req.body
+        if (!password) {
+            return res
+                .status(400)
+                .send({ status: false, message: "please enter password !" });
+        }
+        //-------------------------------------//
 
-    //if not user
-    let user = await userModel.findOne({ email: userName });
-    if (!user) {
-      return res
-        .status(400)
-        .send({ status: false, message: "username is not corerct" });
-    }
-    //if password not correct
-    let pass = await userModel.findOne({ password: password });
-    if (!pass) {
-      return res
-        .status(400)
-        .send({ status: false, message: "password is not corerct" });
-    }
-    //---------------------//
-    //success creation starting
+        //if not user
+        let user = await userModel.findOne({ email: userName });
+        if (!user) {
+            return res
+                .status(400)
+                .send({ status: false, message: "username is not corerct" });
+        }
+        //if password not correct
+        let pass = await userModel.findOne({ password: password });
+        if (!pass) {
+            return res
+                .status(400)
+                .send({ status: false, message: "password is not corerct" });
+        }
+        //---------------------//
+        //success creation starting
 
-    let token = jwt.sign(
-      {
-        userId: user._id.toString(),
-        batch: "project3",
-        organisation: "group37",
-      },
-      "functionup-plutonium",
-      { expiresIn: "24h" }
-    );
+        let token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                batch: "project3",
+                organisation: "group37",
+            },
+            "functionup-plutonium",
+            { expiresIn: "24h" }
+        );
 
-    res.status(200).send({ status: true, message: "Success", data: token });
-  } catch (err) {
-    res.status(500).send({ message: "server error", error: err });
-  }
+        res.status(200).send({ status: true, message: "Success", data: token });
+    } catch (err) {
+        res.status(500).send({ message: "server error", error: err });
+    }
 };
 
 module.exports = { createUser, loginUser };
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  title: { type: String, reqiured: true, enum: ["Mr", "Mrs", "Miss"] },
-  name: { type: String, reqiured: true },
-  phone: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 8, maxlength: 15 },
+  title: { type: String, reqiured: true, enum: ["Mr", "Mrs", "Miss"],trim:true },
+  name: { type: String, reqiured: true ,trim:true},
+  phone: { type: String, required: true, unique: true,trim:true },
+  email: { type: String, required: true, unique: true ,trim:true},
+  password: { type: String, required: true, minlength: 8, maxlength: 15 ,trim:true},
   address: {
-    street: { type: String },
-    city: { type: String },
-    pincode: { type: String },
+    street: { type: String ,trim:true},
+    city: { type: String,trim:true },
+    pincode: { type: String ,trim:true},
   },
 },
   {timestamps: true}
