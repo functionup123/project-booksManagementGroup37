70b7dbbd4cd545a1ddfd40d99abcec5d6a0824c7
Aakash Ghosh <aakash.duke.2019@gmail.com>
2022-09-20 17:02:13 +0530

    2 api done

--- src/route/route.js
+++ src/route/route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const route = express.Router();
+const createUserValiation=require("../middleware/userValidation")
 
 const userController=require('../contoller/userController')
 route.get("/test-me", (req, res) => {
     res.send("My first ever api!");
   });
   
-route.post('/register',userController.createUser)
+route.post('/register',createUserValiation.cuv,userController.createUser)
 route.post('/login', userController.loginUser)
 
 module.exports = route;
