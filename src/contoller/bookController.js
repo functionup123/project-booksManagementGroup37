f5464c33f8fa3711226c7fcf00a5e9666213557f
sabiha <you@example.com>
2022-09-21 20:00:48 +0530

    put done

--- src/contoller/bookController.js
+++ src/contoller/bookController.js
@@ -34,5 +34,100 @@ catch (err) {
 }
 }
 
+///update//
+const updateBook=async function (req,res){
+  try{
+    let bookId=req.params.bookId;
+    if (!bookId)
+    {
+        return res
+       .status(400)
+       .send({status:false,message:"Book id is required"});
+    }
+    let requestBody=req.body;
+    let {title,excerpt,releasedAt,ISBN}=requestBody;
+    if (title)
+    {
+       if (title)
+       {
+         return res
+        .status(400)
+        .send({status:false,message:"Provide a valid Title"});
+       }
+      if (excerpt)
+      {
+        if(excerpt){
+          return res
+          .status(400)
+          .send({Status:false,message:"[provide a valid excerpt"});
+        }
+      }
+      if (userId) 
+      {
+        return res
+          .status(400)
+          .send({ status: false, message: "User Id is required" });
+      }
+      if (ISBN) {
+        return res
+          .status(400)
+          .send({ status: false, message: " ISBN is required" });
+      }
+      if (ISBN) {
+        return res.status(400).send({
+          status: false,
+          message: "  ISBN and should be 10 or 13 digits",
+        });
+      }
+      if (category){
+        return res.status(400).send({
+          status: false,
+          message: "Please provide a category or a Valid category",
+        });
+      }
 
-module.exports={ createBook,getBooks}
+  if (subcategory) {
+      return res.status(400).send({
+        status: false,
+        message: "Please provide a subcategory or a Valid subcategory",
+      });
+    }
+    if (releasedAt) 
+    {
+      return res.status(400).send(
+      {
+        status: false,
+        message: "please provide releaseAt or valid releasedAt",});
+    }
+
+    }
+    let bookUpdated = await bookModel.findOneAndUpdate(
+      { _id: bookId },
+      {
+        $set: {
+          title: requestBody.title,
+          excerpt: requestBody.excerpt,
+          releasedAt: requestBody.releasedAt,
+          ISBN: requestBody.ISBN,
+          category: requestBody.category,
+          subcategory: requestBody.subcategory,
+          reviews:requestBody.reviews,
+          releasedAt:requestBody.releasedAt
+
+        },
+      }, 
+      { new: true }
+    );
+
+    return res.status(200).send({
+      status: true,
+      message: "Book Data Updated Successfully",
+      data: bookUpdated,
+    });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message });
+  }
+};
+
+
+module.exports={ createBook,getBooks,updateBook}
